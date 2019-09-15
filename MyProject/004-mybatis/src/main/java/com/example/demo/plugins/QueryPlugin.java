02a2d39eebb1209dd3b05bda62644f15988b3d3e
fhchen <chenfuhai775@163.com>
2019-09-15 22:44:08 +0800

    提交代码

---
+++ MyProject/004-mybatis/src/main/java/com/example/demo/plugins/QueryPlugin.java
@@ -0,0 +1,30 @@
+package com.example.demo.plugins;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(
+                type = Executor.class,
+                method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}
+        )
+})
+public class QueryPlugin implements Interceptor {
+    public Object intercept(Invocation invocation) throws Throwable {
+        return invocation.proceed();
+    }
+
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    public void setProperties(Properties arg0) {
+    }
+
+}
