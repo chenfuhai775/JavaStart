99788fa10ea127b8f46473243c2d6929f5ae3c8b
fh.chen <aaa>
2019-09-16 10:08:19 +0800

    更新代码

--- MyProject/004-mybatis/src/main/java/com/example/demo/MybatisHelloWorld.java
+++ MyProject/004-mybatis/src/main/java/com/example/demo/MybatisHelloWorld.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import com.example.demo.dao.UserDao;
 import com.example.demo.entity.User;
+
 import java.io.IOException;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,27 +12,27 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class MybatisHelloWorld {
 
-  public static void main(String[] args) {
-    SqlSession sqlSession = getSessionFactory().openSession();
-    UserDao userMapper = sqlSession.getMapper(UserDao.class);
-    User user = new User();
-    user.setContactID(2);
-    user.setOrderNo("C0000002");
-    user = userMapper.findUserById(user);
-    if (user != null) {
-      System.out.println("編號:" + user.getContactID() + ",訂單號:" + user.getOrderNo());
+    public static void main(String[] args) {
+        SqlSession sqlSession = getSessionFactory().openSession();
+        UserDao userMapper = sqlSession.getMapper(UserDao.class);
+        User user = new User();
+        user.setContactID(2);
+        user.setOrderNo("C0000002");
+        user = userMapper.findUserById(user);
+        if (user != null) {
+            System.out.println("編號:" + user.getContactID() + ",訂單號:" + user.getOrderNo() + ",姓:" + user.getSureName());
+        }
     }
-  }
 
-  private static SqlSessionFactory getSessionFactory() {
-    SqlSessionFactory sessionFactory = null;
-    String resource = "configuration.xml";
-    try {
-      sessionFactory = new SqlSessionFactoryBuilder().build(Resources
-          .getResourceAsReader(resource));
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static SqlSessionFactory getSessionFactory() {
+        SqlSessionFactory sessionFactory = null;
+        String resource = "configuration.xml";
+        try {
+            sessionFactory = new SqlSessionFactoryBuilder().build(Resources
+                    .getResourceAsReader(resource));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
     }
-    return sessionFactory;
-  }
 }
--- MyProject/004-mybatis/src/main/java/com/example/demo/entity/User.java
+++ MyProject/004-mybatis/src/main/java/com/example/demo/entity/User.java
@@ -1,12 +1,12 @@
 package com.example.demo.entity;
 
 import java.math.BigInteger;
+
 import lombok.Data;
 
 @Data
 public class User {
-
-  private Integer ContactID;
-
-  private String OrderNo;
+    private Integer ContactID;
+    private String OrderNo;
+    private String SureName;
 }
